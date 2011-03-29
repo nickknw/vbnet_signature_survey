c0f3a1e2250d97e7af2f4a92094edcef8d81c121
Nick <nickknw@gmail.com>
2011-03-29 09:02:53 -0700

    includes the majority of the program this time

--- vb_signature_survey.rb
+++ vb_signature_survey.rb
@@ -7,14 +7,33 @@ $html_style_block = %Q|
 	    body { font-family: georgia, serif; }
 	    h1 { color: #111111 }
 	    li { font-family: Consolas, monospace; font-size: 14px; margin-bottom:20px; list-style-type: none; word-wrap:break-word; }
-	    .line_count { color: #000080; }
-	    .method_count { color: #008080; }
+	    .line_count { color: #000080; font-family: Consolas, monospace;}
+	    .method_count { color: #008080; font-family: Consolas, monospace;}
 	    .signature { font-size: 12px; }
 	    .inside_method { background-color: #eeeeee; }
 	    .detail_page { font-family: Consolas, monospace; font-size: 14px; word-wrap: break-word}
 	    .method_name { background-color: #ccccee ; font-weight: bold; }
-	    .type { background-color: 88ffaa; font-weight: bold; }
+	    .type { background-color: aaffbb; font-weight: bold; }
+	    .inside_if { background-color:#CCEECC; }
+	    .inside_loop { background-color:#EECCCC; }
+	    .inside_loop .inside_if { background-color:#BBBBAA; }
+	    .inside_if .inside_loop { background-color:#CCAAAA; }
+	    .comment { color: #777777; }
+	    p { display: inline; margin: 0px; padding: 0px; }
+	    div { display: inline; margin: 0px; padding: 0px; }
+	    #legend { font-family: Consolas, monospace; font-size: 14px; margin-bottom:10px; }
+	    #legend td { padding-right: 10px; }
 	</style>|
+
+$legend_html = %Q|
+    <table id="legend">
+	<tr><td><span style="font-size:18px; font-family: georgia, serif; font-weight: bold;">Legend</span></td></tr>
+	<tr><td><span class="method_count">14m</span> means 14 methods</td> <td><span class="line_count">294L</span> means 294 lines</td> </tr>
+	<tr><td>. is a (non-blank) line</td> <td>' is a comment</td> </tr>
+	<tr><td><span class='inside_if'>\|</span> is a single line If statement</td> <td><div class='inside_method'>{Inside a method}</div></td></tr>
+	<tr><td> <span class='inside_if'>(Inside an if block)</span></td><td> <p class='inside_loop'>[Inside a loop]</p></td></tr>
+	<tr><td> <p class='inside_loop'><span class='inside_if'>[(An if block inside a loop)]</span></p></td><td> <span class='inside_if'><p class='inside_loop'>([A loop inside an if block])</p></span></td></tr>
+    </table>|
 
 def for_each_vb_file_in_dir(directory, reportfile, method_to_execute)
     current_directory = Dir.new(directory)
@@ -69,11 +88,15 @@ def vb_file_report(filename)
 	    
 	    #extract only relevant text
 	    detail_page_text = ""
+	    continued_function_sig = false;
             f.each_line { |line|
         	lines += 1
 
 		#begin VB-specific code
-        	if line =~ /#Region/
+		if continued_function_sig
+        	    detail_page_text.concat indent[] + line.strip.gsub(/</, "&lt;").gsub(/>/, "&gt;") + "<br>" + indent[] 
+		    continued_function_sig = false unless line =~ /_$/ 
+        	elsif line =~ /#Region/
         	    detail_page_text.concat "<br>" + indent[] + line.strip.gsub(/</, "&lt;").gsub(/>/, "&gt;") + "<br>" + indent[]
         	    indent_level += 1
 		    signature += "."
@@ -84,12 +107,36 @@ def vb_file_report(filename)
 		elsif line =~ /(End\s+Sub|End\s+Function)/
         	    detail_page_text.concat "<br>" + indent[] + line.strip.gsub(/</, "&lt;").gsub(/>/, "&gt;") + "<br>" + indent[] 
 		    signature += "}"
-        	elsif line =~ /(Sub\s|Function\s)/
+		elsif line =~ /Exit Sub/
+		    #ugly special case; treat like normal line
+        	    detail_page_text.concat "."
+		    signature += "."
+        	elsif line =~ /(\bSub\s|\bFunction\s)/
         	    methods += 1
         	    detail_page_text.concat "<br>" + indent[] + line.strip.gsub(/</, "&lt;").gsub(/>/, "&gt;") + "<br>" + indent[] 
 		    signature += "{"
+		    continued_function_sig = true if line =~ /_$/ 
+		elsif line =~ /^\s+'/
+		    signature += "'"
+		    detail_page_text.concat "'"
+		elsif line =~ /Next|End\s+While/
+		    signature += "]"
+		    detail_page_text.concat "]"
+		elsif line =~ /For Each|While/
+		    signature += "["
+		    detail_page_text.concat "["
+		elsif line =~ /End\s+If|End Select/
+		    signature += ")"
+		    detail_page_text.concat ")"
+		elsif line =~ /[^e]If.*Then\s*$|Select Case/
+		    signature += "("
+		    detail_page_text.concat "("
+		elsif line =~ /[^e]If/
+		    signature += "|"
+		    detail_page_text.concat "|"
 		elsif line =~ /^\s*$/
-		    #ignore blank lines
+        	    detail_page_text.concat " "
+		    signature += " "
         	else
         	    detail_page_text.concat "."
 		    signature += "."
@@ -105,19 +152,46 @@ def vb_file_report(filename)
 	    lines_html = " <span class='line_count'>" + " #{lines}L".ljust(8).gsub(' ', "&nbsp;") + "</span>"
 	    file_name_html = "<a href=\"#{filename + ".htm"}\">#{just_name_of_file}</a>#{spaces_in_justified_name} "
 
-	    signature_html = signature.gsub(/{/, "<span class='inside_method'>{")
-	    signature_html = signature_html.gsub(/}/, "}</span>")
+	    signature_html = signature
+	    signature_html.gsub!(/'/, "<span class='comment'>'</span>")
+	    signature_html.gsub!(/{/, "<div class='inside_method'>{")
+	    signature_html.gsub!(/}/, "}</div>")
+
+	    signature_html.gsub!(/\[/, "<p class='inside_loop'>[")
+	    signature_html.gsub!(/\]/, "]</p>")
+
+	    signature_html.gsub!(/\(/, "<span class='inside_if'>(")
+	    signature_html.gsub!(/\)/, ")</span>")
+	    signature_html.gsub!(/\|/, "<span class='inside_if'>|</span>")
+
 	    signature_html = "<span class='signature'>#{signature_html}</span>"
 
 	    file_summary_line = methods_html + lines_html + file_name_html + signature_html
 
 	    #write text to html file
-	    g.puts %Q|<html><head><title>#{just_name_of_file}#{$html_style_block}</head><body><div class='detail_page'>|
-	    g.puts "<h2>" + just_name_of_file + ": " + methods_html + lines_html + "</h2>"
+	    file_path_without_extension = filename.sub(/(\/.*?)\..+$/, "\\1")
+	    g.puts %Q|<html><head><title>#{file_path_without_extension}</title>#{$html_style_block}</head><body>|
+	    g.puts "<h2>" + file_path_without_extension + ": " + methods_html + lines_html + "</h2><div class='detail_page'>"
+	    g.puts $legend_html
 
-	    detail_page_text.gsub!(/\s/, "&nbsp;")
+	    detail_page_text.gsub!(/'/, "<span class='comment'>'</span>")
+	    detail_page_text.gsub!(/>\s*\(\) (As|Handles)/, ">&#40;&#41; \\1")
+	    detail_page_text.gsub!(/\) (As|Handles)/, "&#41; \\1")
+	    detail_page_text.gsub!(/\s\s/, "&nbsp;&nbsp;")
 	    detail_page_text.gsub!(/\b\w+\s?(?=\()/, "<span class='method_name'>\\0</span>")
-	    detail_page_text.gsub!(/As&nbsp;(\w+\.)*\w+/, "<span class='type'>\\0</span>")
+	    detail_page_text.gsub!(/>\s*\((ByVal|ByRef)/, ">&#40;\\1")
+	    detail_page_text.gsub!(/As (\w+\.)*\w+/, "<span class='type'>\\0</span>")
+
+
+	    detail_page_text.gsub!(/{/, "<div class='inside_method'>{")
+	    detail_page_text.gsub!(/}/, "}</div>")
+
+	    detail_page_text.gsub!(/\[/, "<p class='inside_loop'>[")
+	    detail_page_text.gsub!(/\]/, "]</p>")
+
+	    detail_page_text.gsub!(/\(/, "<span class='inside_if'>(")
+	    detail_page_text.gsub!(/\)/, ")</span>")
+	    detail_page_text.gsub!(/\|/, "<span class='inside_if'>|</span>")
 
 	    g.puts detail_page_text
 
@@ -141,8 +215,10 @@ report_file_name_ = "#{$root_directory_name}-report-vb.htm"
 File.open($signature_survey_directory + "/" + report_file_name_, "w+") { |report_file|
     puts "Using: #{File.expand_path(".")}"
     puts ""
-    report_file.puts %Q|<html><head>#{$html_style_block}</head><body>|
+    report_file.puts %Q|<html><head><title>VB Signature Survey</title>#{$html_style_block}</head><body>|
+    report_file.puts "<h1>VB Signature Survey</h1>"
     report_file.puts "<h2>Using: #{File.expand_path(".")}</h2>"
+    report_file.puts $legend_html
     
     for_each_vb_file_in_dir(".", report_file, method(:vb_file_report))
 
